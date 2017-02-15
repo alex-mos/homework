5f9615264d5536272500e38213a75060bda7f101
Alex Mos <alexander.mospan@gmail.com>
2017-02-15 20:01:21 +0300

    a little

--- lesson1/server.js
+++ lesson1/server.js
@@ -4,7 +4,7 @@ console.log('Server started')
 
 var server = http.createServer(function(request, response) {
     console.log('request accepted')
-    response.end('hello docker!!!')
+    response.end('hello docker.')
 })
 
 server.listen(80)
